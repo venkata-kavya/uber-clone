e0d2bef78bb9b84f7d7382cfbcdad280601d4f65
Kavya <venkatakavya734@gmail.com>
2025-04-23 19:58:47 +0530

    Create register route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,1 +1,10 @@
+const { validationResult } = require("express-validator");
 const userModel = require("../models/user.model");
+const userService = require("../services/user.service");
+
+module.exports.registerUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+};

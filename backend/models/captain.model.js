b127912666b4363a79d3d961312b6e3e9e14527a
Kavya <venkatakavya734@gmail.com>
2025-04-29 20:43:57 +0530

    Add captain schema methods

--- backend/models/captain.model.js
+++ backend/models/captain.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const captainSchema = new mongoose.Schema({
   fullname: {
@@ -64,3 +66,22 @@ const captainSchema = new mongoose.Schema({
     },
   },
 });
+
+captainSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+  return token;
+};
+
+captainSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+captainSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+const captainModel = mongoose.model("captain", captainSchema);
+
+module.exports = captainModel;

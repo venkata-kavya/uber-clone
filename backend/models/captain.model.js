5bf4ca8a7464ea8d60b0c9e0734d6fc4932c0ecb
Kavya <venkatakavya734@gmail.com>
2025-04-29 20:37:40 +0530

    Create captain model

---
+++ backend/models/captain.model.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+const captainSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "Firstname must be atleast 3 characters long"],
+    },
+    lastname: {
+      type: String,
+      minlength: [3, "Lastname must be atleast 3 characters long"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
+    minlength: [5, "Email must be atleast 5 characters long"],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  socketId: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "inactive",
+  },
+  vehicle: {
+    color: {
+      type: String,
+      required: true,
+      minlength: [3, "Color must be atleast 3 characters long"],
+    },
+    plate: {
+      type: String,
+      required: true,
+      minlength: [3, "Color must be atleast 3 characters long"],
+    },
+    capacity: {
+      type: Number,
+      required: true,
+      min: [1, "Capacity must be atleast 1"],
+    },
+    vehicleType: {
+      type: String,
+      required: true,
+      enum: ["motorcycle", "car", "auto"],
+    },
+  },
+  location: {
+    lat: {
+      type: Number,
+    },
+    lng: {
+      type: Number,
+    },
+  },
+});

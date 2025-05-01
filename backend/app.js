50e82389b8e03a8cd920fb6e72c5e80296bc7a32
Kavya <venkatakavya734@gmail.com>
2025-05-01 19:06:52 +0530

    Create captain register route

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,9 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
+
 const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 
 connectToDb();
 
@@ -19,5 +21,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
 
 module.exports = app;

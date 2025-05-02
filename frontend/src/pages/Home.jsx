c8f8852c9a53f838559706122f628258a7d2946a
Kavya <venkatakavya734@gmail.com>
2025-05-02 20:26:48 +0530

    Create Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import uberLogo from "../assets/uberLogo.png";
+import trafficLight from "../assets/trafficLight.avif";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div
+        className=" h-screen pt-8 w-full flex flex-col justify-between bg-red-400"
+        style={{
+          backgroundImage: `url(${trafficLight})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <img className="w-16 ml-8" src={uberLogo} alt="uber-logo" />
+        <div className="bg-white pb-7 px-4 py-4">
+          <h2 className="text-3xl font-bold">Get Started with Uber</h2>
+          <Link
+            to="/login"
+            className="flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5"
+          >
+            Continue
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import uberLogo from "../assets/uberLogo.png";
 
-const Login = () => {
-  return <div>Login</div>;
+const UserLogin = () => {
+  return (
+    <div className="p-7">
+      <form>
+        <img className="w-16 mb-10 ml-1" src={uberLogo} alt="uber-logo" />
+        <h3 className="text-lg font-inter font-medium mb-2">
+          What's your email
+        </h3>
+        <input
+          required
+          className="bg-[#eeeeee] mb-7 rounded px-4 mt-1 py-2  w-full text-lg placeholder:text-base"
+          type="email"
+          placeholder="email@example.com"
+        ></input>
+        <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+        <input
+          required
+          className="bg-[#eeeeee] mb-7 rounded px-4 mt-1 py-2  w-full text-lg placeholder:text-base"
+          type="password"
+          placeholder="password"
+        ></input>
+        <button className="bg-[#111] text-[#fff] font-semibold mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
-export default Login;
-Login;
+export default UserLogin;

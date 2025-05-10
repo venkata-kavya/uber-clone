41fdb84d445eb5483d6357ad84f2d49ce3885ce8
Kavya <venkatakavya734@gmail.com>
2025-05-10 19:44:50 +0530

    Integrate Captain Login endpoint

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
 import uberDriver from "../assets/uberDriver.png";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { CaptainDataContext } from "../context/CaptainContext";
 
-const CaptainLogin = () => {
+const Captainlogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [captainData, setCaptainData] = useState({});
 
-  const submitHandler = (e) => {
+  const { captain, setCaptain } = React.useContext(CaptainDataContext);
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-
-    setCaptainData({
+    const captain = {
       email: email,
-      password: password,
-    });
+      password,
+    };
+
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/captains/login`,
+      captain
+    );
+
+    if (response.status === 200) {
+      const data = response.data;
+
+      setCaptain(data.captain);
+      localStorage.setItem("token", data.token);
+      navigate("/captain-home");
+    }
+
     setEmail("");
     setPassword("");
   };
@@ -21,37 +39,43 @@ const CaptainLogin = () => {
   return (
     <div className="p-7 h-screen flex flex-col justify-between">
       <div>
+        <img className="w-20 mb-3" src={uberDriver} alt="" />
+
         <form
           onSubmit={(e) => {
             submitHandler(e);
           }}
         >
-          <img className="w-16 mb-10 ml-1" src={uberDriver} alt="uber-logo" />
-          <h3 className="text-lg font-inter font-medium mb-2">
-            What's your email
-          </h3>
+          <h3 className="text-lg font-medium mb-2">What's your email</h3>
           <input
             required
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-[#eeeeee] mb-7 rounded px-4 mt-1 py-2  w-full text-lg placeholder:text-base"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className="bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
             type="email"
             placeholder="email@example.com"
-          ></input>
+          />
+
           <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+
           <input
-            required
+            className="bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="bg-[#eeeeee] mb-7 rounded px-4 mt-1 py-2  w-full text-lg placeholder:text-base"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
             type="password"
             placeholder="password"
-          ></input>
-          <button className="bg-[#111] text-[#fff] font-semibold mb-5 rounded px-4 py-2  w-full text-lg placeholder:text-base">
+          />
+
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base">
             Login
           </button>
         </form>
-        <p className="text-center mb-2">
+        <p className="text-center">
           Join a fleet?{" "}
           <Link to="/captain-signup" className="text-blue-600">
             Register as a Captain
@@ -60,8 +84,8 @@ const CaptainLogin = () => {
       </div>
       <div>
         <Link
-          to="/signup"
-          className="bg-[#d5622d] flex items-center justify-center text-[#fff] font-semibold mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base"
+          to="/login"
+          className="bg-[#d5622d] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base"
         >
           Sign in as User
         </Link>
@@ -70,4 +94,4 @@ const CaptainLogin = () => {
   );
 };
 
-export default CaptainLogin;
+export default Captainlogin;
